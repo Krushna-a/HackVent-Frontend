94f71c70fb472d0466e743d0c86d2ac15ea0ae7d
Krushna-a <Krushnaboinwad70@gmail.com>
2025-06-27 17:57:35 +0530

    loading bar added

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect,useContext } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBuildingColumns,
